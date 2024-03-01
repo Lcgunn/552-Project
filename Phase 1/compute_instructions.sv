cbceedeb9804f80ef360be1b1e1ce82cae3a8dea
Savannah712 <118080545+Savannah712@users.noreply.github.com>
2024-03-01 16:59:49 -0600

    Update compute_instructions.sv
    Moving testRED to testbench file

--- Phase 1/compute_instructions.sv
+++ Phase 1/compute_instructions.sv
@@ -51,23 +51,7 @@ carry_look_ahead CLA3 [2:0] (.Sum(interSum), .Ovfl(), .A({{3{totalsumAB[7]}}, to
 
 endmodule
 
-module RED_tb();
-logic signed [15:0] rs, rt;
-logic [15:0] Sum;
 
-RED redDUT (.rs(rs), .rt(rt), .Sum(Sum));
-
-initial begin
-	rs = 16'h1111;
-	rt = 16'h1111;
-	#5
-	if(Sum != 16'h2222) begin	
-		$display("Sum is %h, when it should be %h", Sum, 16'h2222);
-		$stop();
-	end
-	#5;
-end
-endmodule
 
 
 
