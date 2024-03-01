c08c6c2d4930bba747e441a93860d37cc0884b9b
Savannah712 <118080545+Savannah712@users.noreply.github.com>
2024-03-01 17:00:29 -0600

    Update testbench.sv
    add testbench for RED and change name to t_RED

--- Phase 1/testbench.sv
+++ Phase 1/testbench.sv
@@ -1,1 +1,17 @@
+module t_RED();
+logic signed [15:0] rs, rt;
+logic [15:0] Sum;
 
+RED redDUT (.rs(rs), .rt(rt), .Sum(Sum));
+
+initial begin
+	rs = 16'h1111;
+	rt = 16'h1111;
+	#5
+	if(Sum != 16'h2222) begin	
+		$display("Sum is %h, when it should be %h", Sum, 16'h2222);
+		$stop();
+	end
+	#5;
+end
+endmodule
