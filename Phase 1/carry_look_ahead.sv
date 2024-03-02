dc881f6611bf68f9f9c5ac5dee2461e1cf1fad59
lmyszko <119759329+lmyszko@users.noreply.github.com>
2024-03-01 18:19:08 -0600

    Update carry_look_ahead.sv

--- Phase 1/carry_look_ahead.sv
+++ Phase 1/carry_look_ahead.sv
@@ -1,20 +1,18 @@
 
 //Instantiates full_adder_1bit to make it 4 wide
 //overflow is determined by seperate logic than carry out
-module carry_look_ahead (Sum, Ovfl, A, B, pad, cin, Cout);
+module carry_look_ahead (Sum, Ovfl, A, B, pad, Cin, Cout);
 	input pad; //pad-notpad indicator
 	input [3:0] A, B; //Input values
-	input cin;
+	input Cin;
 	output [3:0] Sum; //sum output
 	output Ovfl; //To indicate overflow
 	output Cout;
-	wire ovfl;
 	wire interCin;
 	wire [3:0] interC;
-	wire [3:0] interB;
 	wire [3:0] interSum;
 
-	assign interCin = (pad) ? '0 : cin;
+	assign interCin = (pad) ? '0 : Cin;
 	wire [3:0] P, G;
 
 	// Get propagate for each bit
@@ -33,12 +31,12 @@ module carry_look_ahead (Sum, Ovfl, A, B, pad, cin, Cout);
 	//assign Cout[0] = G[0] ^ (P[0]&Cin);
 	//assign Cout[1] = G[1] ^ (P[1]&G[0]) ^(P[1]&P[0]&Cin);
 	//assign Cout[2] = G[2] ^ (P[2]&G[1]) ^ (P[2]&P[1]&G[0]) ^(P[2]P[1]&P[0]&Cin);
-	assign Cout = G[3] | (P[3]&G[2]) | (P[3]&P[2]&G[1]) | (P[3]&P[2]&P[1]&G[0]) | (P[3]&P[2]&P[1]&P[0]&cin);
+	assign Cout = G[3] | (P[3]&G[2]) | (P[3]&P[2]&G[1]) | (P[3]&P[2]&P[1]&G[0]) | (P[3]&P[2]&P[1]&P[0]&interCin);
 
 	//note interB is not technically full2s compliment, but its close enough for the calculation needed
-	assign Ovfl = (interSum[3] ? (~A[3]&~interB[3]) : (A[3] & interB[3])); 
+	assign Ovfl = (interSum[3] ? (~A[3]&~B[3]) : (A[3] & B[3])); 
 	// Saturating arithmetic
 	assign Sum = (pad)? ((Ovfl) ? ((~A[3])? 4'b0111 : 4'b1000): interSum) : interSum;
 
-	full_adder_1bit FA [3:0]  (.A(A),.B(interB),.Cin({interC[2:0],interCin}),.Cout(interC),.S(interSum));
+	full_adder_1bit FA [3:0]  (.A(A),.B(B),.Cin({interC[2:0],interCin}),.Cout(interC),.S(interSum));
 endmodule
