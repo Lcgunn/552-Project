ea531b103364ac4f872a92b9f569433b4c4b9080
Lcgunn <118074921+Lcgunn@users.noreply.github.com>
2024-03-02 16:10:32 -0600

    Updating ALU and added comments to shifter

--- Phase 1/ALU.sv
+++ Phase 1/ALU.sv
@@ -1,61 +1,78 @@
-module ALU (output [15:0] ALU_Out, output [2:0] flags, input [3:0] opcode, input [15:0] operand1, input [15:0] operand2);
-	wire[15:0] inter_adder, inter_shift, inter_RED, ALU_Out;
-	
+module ALU (output [15:0] ALU_Out, output [2:0] flags, input [3:0] opcode, input [15:0] operand1, input [15:0] operand2, input clk, input rst);
+	wire[15:0] inter_adder;		// Output for adder (LW & SW too)
+	wire [15:0] inter_shift;	// Output for shift
+	wire [15:0] inter_RED;		// Output for red
+
 	wire [3:0] shift_mode;
-	reg [2:0] flags;//OVERFLOW[2]. NEGATIVE [1], ZERO IS [0]
+	reg [2:0] flaginput;//OVERFLOW[2]. NEGATIVE [1], ZERO IS [0]
 	reg [2:0] flag_enable;
-	wire temp_Ovfl
-	wire sub;
-	PSA_16bit adder (.Sum(inter_adder),.Ovfl(temp_Ovfl), .A(operand1),.B(operand2),.Sub(sub),.pad());//FIXME SUB should be implemented
-	//Operand2 [3;0]
-	Shifter shift(.Shift_out(inter_shift),.Shift_In(operand1),.Shift_Val(operand2[3:0]),.Mode(shift_mode));
+	wire [15:0] inter_operand2;
+	wire temp_Ovfl;
+	reg isub, ipad;
+	// Implementing ADD/SUB/PADDSB
+	PSA_16bit adder (.Sum(inter_adder),.Ovfl(temp_Ovfl), .A(operand1),.B(inter_operand2),.sub(isub),.pad(ipad));//FIXME SUB should be implemented
 	
-	dff Conditions [2:0] (.q(flags),.d(), .wen(flag_enable), .clk(clk), rst)
+	// Implementing SLL/SRA/ROR
+	Shifter shift(.Shift_out(inter_shift),.Shift_In(operand1),.Shift_Val(operand2[3:0]),.Mode(shift_mode));
+
+	// Implementing RED
+	RED red (.rs(operand1), .rt(operand2), .Sum(inter_RED)); 
+	
+	dff Conditions [2:0] (.q(flags),.d(flaginput), .wen(flag_enable), .clk(clk), .rst(rst));
 	//RED
 	reg error;
 	always @ opcode begin
-	flags = '0;
+	isub = '0;
+	ipad = '0;
+	flag_enable = '0;
+	inter_operand2 = operand2;
 		case (opcode)
 		(4'h0000): //Add
 			ALU_Out = inter_adder;
-			flags[0]= ~(|ALU_Out);
-			flags[1]= ALU_Out[15];
-			flags[2] = temp_Ovfl;
+			flaginput[0]= ~(|ALU_Out);	// If all bits are zero flag is 1
+			flaginput[1]= ALU_Out[15];	// If MSB of sum is 1, then negative
+			flaginput[2] = temp_Ovfl;	// If overflow, then overflow flag is 1
 			error = 1'b0;
 		(4'h0001): //Sub
+			sub = '1;
 			ALU_Out = inter_adder;
-			flags[0]= ~(|ALU_Out);
-			flags[1]= ALU_Out[15];
-			flags[2] = temp_Ovfl;
+			flaginput[0]= ~(|ALU_Out);	// If all bits are zero flag is 1
+			flaginput[1]= ALU_Out[15];	// If MSB of sum is 1, then negative
+			flaginput[2] = temp_Ovfl;	// If overflow, then overflow flag is 1
 			error = 1'b0;
 		(4'h0010): //XOR
-			ALU_Out = operand1 ^ operand2
-			flags[0]= |ALU_Out;
+			ALU_Out = operand1 ^ operand2;
+			flaginput[0]= ~(|ALU_Out);		// If all bits are zero flag is 1
 			error = 1'b0;
 		(4'h0011): //RED
-			//FIXME TODO Implemet RED
+			ALU_Out = inter_RED;
 			error = 1'b0;
 		(4'h0100): //SLL
+			shift_mode = 2'b00;
 			ALU_Out = inter_shift;
-			shift_mode = 00;
-			flags[0]= |ALU_Out;
+			flaginput[0]= ~(|ALU_Out);	// If all bits are zero flag is 1
 			error = 1'b0;
 		(4'h0101): //SRA
-		flags[0]= |ALU_Out;
+			shift_mode = 2'b01;
+			ALU_Out = inter_shift;
+			flaginput[0]= ~(|ALU_Out);	// If all bits are zero flag is 1
 			error = 1'b0;
 		(4'h0110): //ROR
-		flags[0]= |ALU_Out;
+			shift_mode = 2'b10;			// Mode can be 10 or 11
+			ALU_Out = inter_shift;
+			flaginput[0]= ~(|ALU_Out);	// If all bits are zero flag is 1
 			error = 1'b0;
 		(4'h0111): //PADDSB
-		
+			pad = '1;
 			error = 1'b0;
 		(4'h1000): //LW
-			
+			inter_operand2 = operand2 << 1;
+			ALU_Out = inter_adder;
 			error = 1'b0;
 		(4'h1001): //SW
+			inter_operand2 = operand2 << 1;
+			ALU_Out = inter_adder;
 			error = 1'b0;
-			
-			
 		(4'h1010): //LLB
 			error = 1'b0;
 		(4'h1011): //LHB
