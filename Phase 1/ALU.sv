94f9211ba3b288e5a33f89e7622bdb7cdd425ec4
lmyszko <119759329+lmyszko@users.noreply.github.com>
2024-03-02 14:58:35 -0600

    Update ALU.sv for Leah to start at

--- Phase 1/ALU.sv
+++ Phase 1/ALU.sv
@@ -3,11 +3,14 @@ module ALU (output [15:0] ALU_Out, output [2:0] flags, input [3:0] opcode, input
 	
 	wire [3:0] shift_mode;
 	reg [2:0] flags;//OVERFLOW[2]. NEGATIVE [1], ZERO IS [0]
+	reg [2:0] flag_enable;
 	wire temp_Ovfl
 	wire sub;
 	PSA_16bit adder (.Sum(inter_adder),.Ovfl(temp_Ovfl), .A(operand1),.B(operand2),.Sub(sub),.pad());//FIXME SUB should be implemented
 	//Operand2 [3;0]
 	Shifter shift(.Shift_out(inter_shift),.Shift_In(operand1),.Shift_Val(operand2[3:0]),.Mode(shift_mode));
+	
+	dff Conditions [2:0] (.q(flags),.d(), .wen(flag_enable), .clk(clk), rst)
 	//RED
 	reg error;
 	always @ opcode begin
@@ -15,13 +18,13 @@ module ALU (output [15:0] ALU_Out, output [2:0] flags, input [3:0] opcode, input
 		case (opcode)
 		(4'h0000): //Add
 			ALU_Out = inter_adder;
-			flags[0]= |ALU_Out;
+			flags[0]= ~(|ALU_Out);
 			flags[1]= ALU_Out[15];
 			flags[2] = temp_Ovfl;
 			error = 1'b0;
 		(4'h0001): //Sub
 			ALU_Out = inter_adder;
-			flags[0]= |ALU_Out;
+			flags[0]= ~(|ALU_Out);
 			flags[1]= ALU_Out[15];
 			flags[2] = temp_Ovfl;
 			error = 1'b0;
@@ -44,6 +47,7 @@ module ALU (output [15:0] ALU_Out, output [2:0] flags, input [3:0] opcode, input
 		flags[0]= |ALU_Out;
 			error = 1'b0;
 		(4'h0111): //PADDSB
+		
 			error = 1'b0;
 		(4'h1000): //LW
 			
