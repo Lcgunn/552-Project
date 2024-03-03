5ad3543c8847d2e864282000e7fd21d6f68bb698
Lcgunn <118074921+Lcgunn@users.noreply.github.com>
2024-03-03 15:42:46 -0600

    comments!

--- Phase 1/PC_control.sv
+++ Phase 1/PC_control.sv
@@ -2,13 +2,15 @@
 module PC_control(input [2:0]C, input [15:0] I, input [2:0] F, input [1:0] branch, input [15:0] PC_in, output [15:0] PC_out);
 	//Overflow is [2], Negative[1], Zero[0]	
 	reg [15:0] calculated_pc,normal_pc, inter_PC_out;
-	reg [15:0] inter_I;
-
-	// branch = x0: I<<1, branch = x1: I
-	assign inter_I = (branch[0])? I : (I << 1'd1);
+	reg [15:0] inter_op2, inter_op1;
+	//When branch [0] == 1, we are doing the B instruction. When branch [0] == 0, we are doing the BR (Branch Register) instruction 
+	// branch = x0: I<<1, branch = x1: rs
+	assign inter_op2 = (branch[0])? I : (I << 1'd1);
+	// branch = x0: normal_pc, branch = x1: 0 to jump to rs
+	assign inter_op1 = (branch[0])? '0 : normal_pc;
 	
 	PSA_16bit normal (.Sum(normal_pc),.Ovfl(), .A(PC_in),. B(16'h0002),.Sub(0),.pad(0));
-	PSA_16bit immediate (.Sum(calculated_pc),.Ovfl(), .A(normal_pc),. B(inter_I),.Sub(0),.pad(0));
+	PSA_16bit immediate (.Sum(calculated_pc),.Ovfl(), .A(inter_op1),. B(inter_op2),.Sub(0),.pad(0));
 	
 	// If branch instruction, take the pc decided, else go to next pc address
 	// branch = 1x: branch instruction, branch = 0x: not a branch instruction
