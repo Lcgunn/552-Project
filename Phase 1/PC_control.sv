48ad3859bcbbdcda92d390c994fdca76c307f7b0
lmyszko <119759329+lmyszko@users.noreply.github.com>
2024-03-03 15:05:18 -0600

    Update PC_control.sv

--- Phase 1/PC_control.sv
+++ Phase 1/PC_control.sv
@@ -1,3 +1,4 @@
+`default_nettype none
 module PC_control(input [2:0]C, input [8:0] I, input [2:0] F, input [15:0] PC_in, output reg [15:0] PC_out);
 	//Overflow is [2], Negative[1], Zero[0]	
 	//wire [2:0] C;
@@ -5,27 +6,28 @@ module PC_control(input [2:0]C, input [8:0] I, input [2:0] F, input [15:0] PC_in
 	//wire [2:0] F;
 	reg [15:0] calculated_pc,normal_pc;
 	
-	PSA_16bit normal (.Sum(normal_pc),.Ovfl(), .A(PC_in),. B(operand2),.Sub(0),.pad(0));
+	PSA_16bit normal (.Sum(normal_pc),.Ovfl(), .A(PC_in),. B(16'h0002),.Sub(0),.pad(0));
 	PSA_16bit immediate (.Sum(calculated_pc),.Ovfl(), .A(normal_pc),. B(I << 1),.Sub(0),.pad(0));
 	
+	//Watch out for the reset
 	always @ (C,I,F) begin
 	//Overflow is [2], Negative[1], Zero[0]
 		case(C) 
-			(3'b000): //Not Equal
+			3'b000: //Not Equal
 				PC_out = ~F[0] ? calculated_pc : normal_pc; 	
-			(3'b001): //Equal
+			3'b001: //Equal
 				PC_out = F[0] ? calculated_pc : normal_pc;
-			(3'b010): //Greater Than
+			3'b010: //Greater Than
 				PC_out = (~F[0] & ~F[1]) ? calculated_pc : normal_pc;	
-			(3'b011): //Less Than
+			3'b011: //Less Than
 				PC_out = F[1] ? calculated_pc : normal_pc;
-			(3'b100): //Greater Than or Equal
+			3'b100: //Greater Than or Equal
 				PC_out = (F[0] | (~F[0] & ~F[1])) ? calculated_pc : normal_pc;	
-			(3'b101): //Less Than or Equal
+			3'b101: //Less Than or Equal
 				PC_out = (F[0] | F[1]) ? calculated_pc : normal_pc;			
-			(3'b110): //Overflow
+			3'b110: //Overflow
 				PC_out = (F[2]) ? calculated_pc : normal_pc;	
-			(3'b111): //Unconditional
+			3'b111: //Unconditional
 				PC_out = calculated_pc;
 		endcase
 	end
